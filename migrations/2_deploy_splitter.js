b8c5255d339ff26caee2cb389f7b70386f5a99bc
Shebin John <admin@remedcu.com>
2021-03-14 03:43:47 +0530

    Removed the console log of accounts from migration script

--- migrations/2_deploy_splitter.js
+++ migrations/2_deploy_splitter.js
@@ -2,6 +2,5 @@ const Splitter = artifacts.require("Splitter");
 
 module.exports = function(deployer, network, accounts) {
   console.log("network:", network);
-  console.log("accounts:", accounts);
   deployer.deploy(Splitter);
 };
