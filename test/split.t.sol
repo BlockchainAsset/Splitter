69c9dfea426740e219ab2420f98b32e453011668
Shebin John <admin@remedcu.com>
2024-02-10 14:00:23 +0530

    Updated Test

--- test/split.t.sol
+++ test/split.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.23;
 
 import "forge-std/Test.sol";
 import "../src/Splitter.sol";
@@ -60,7 +60,7 @@ contract SplitterTest is Test {
 	}
 
 	function testSplitZeroAddress() public {
-		vm.expectRevert();
+		vm.expectRevert(bytes4(keccak256("ZeroAddressNotAllowed()")));
 		vm.prank(alice);
 		splitter.split{ value: amount }(address(0), address(0));
 	}
